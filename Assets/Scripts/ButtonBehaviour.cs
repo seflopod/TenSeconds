66949aad217c60d5e39253e3bd1cf97cedc9591e
seflopod <bartoschp@gmail.com>
2013-08-24 10:22:45 -0500

    Project added

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+	private const int LEFT_MOUSE_BUTTON = 0;
+	
+	public bool yesButton;
+	public Vector3 _scaled = new Vector3(0.85f, 1.0f, 0.85f);
+	private bool _mouseDownOnMe = false;
+	private Camera _cam;
+	
+	private void Start()
+	{
+		_cam = Camera.main;
+	}
+	
+	void Update()
+	{
+		
+		if(_mouseDownOnMe && Input.GetMouseButtonUp(LEFT_MOUSE_BUTTON))
+		{
+			GameManager.Instance.Clicked(yesButton);
+			transform.localScale = Vector3.one;
+			_mouseDownOnMe = false;
+		}
+		else if(!_mouseDownOnMe && Input.GetMouseButtonDown(LEFT_MOUSE_BUTTON))
+		{
+			Ray mouseRay = _cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(mouseRay, out hit, 10.0f) && hit.collider.gameObject == gameObject)
+			{
+				_mouseDownOnMe = true;
+				transform.localScale = _scaled;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TimerBehaviour.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class TimerBehaviour : MonoBehaviour {
+	private float _timeRemaining;
+	private GUIText _guiText;
+	// Use this for initialization
+	void Start ()
+	{
+		_timeRemaining = 10.0f;
+		_guiText = gameObject.GetComponent<GUIText>();
+	}
+	
+	void FixedUpdate()
+	{
+		_timeRemaining-=Time.fixedDeltaTime;
+		int dispTime = Mathf.CeilToInt(_timeRemaining);
+		_guiText.text = string.Format("0:{0:00}",dispTime);
+		if(_timeRemaining <= 0.0f)
+			GameManager.Instance.GameOver(false);
+	}
+	
+	public void Reset()
+	{
+		_timeRemaining = 10.0f;
+	}
+}
